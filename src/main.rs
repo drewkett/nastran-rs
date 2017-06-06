bb67e5b8441da2ab103bd5149f902a4620774e8b
Andrew Burkett <burkett.andrew@gmail.com>
2017-06-06 11:56:01 -0700

    Add option to output to file

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ extern crate error_chain;
 extern crate dtoa;
 extern crate clap;
 
+use std::fs::File;
+use std::io::Write;
+
 use memmap::{Mmap, Protection};
 use clap::{Arg, App};
 
@@ -27,32 +30,47 @@ mod errors;
 
 pub fn main() {
     let matches = App::new("Nastran Reader")
-        .arg(Arg::with_name("DATFILE")
-                 .help(".dat file for reading")
-                 .required(true)
-                 .index(1))
-        .arg(Arg::with_name("echo")
-                               .long("echo")
-                               .help("Output cards"))
-        .arg(Arg::with_name("new")
-                               .long("new")
-                               .help("Use new parser"))
+        .arg(Arg::with_name("DATFILE").help(".dat file for reading").required(true).index(1))
+        .arg(Arg::with_name("OUTPUT").help("output to file").short("o").takes_value(true))
+        .arg(Arg::with_name("echo").long("echo").help("Output cards"))
+        .arg(Arg::with_name("new").long("new").help("Use new parser"))
         .get_matches();
     if let Some(filename) = matches.value_of("DATFILE") {
         let f = Mmap::open_path(filename, Protection::Read).unwrap();
         let sl = unsafe { f.as_slice() };
+        let echo = matches.is_present("echo") || matches.is_present("OUTPUT");
         if matches.is_present("new") {
             let deck = datfile2::parse_buffer(sl).unwrap();
-            if matches.is_present("echo") {
-                for card in deck.cards {
-                    println!("{}",card)
+            if echo {
+                if let Some(output_filename) = matches.value_of("OUTPUT") {
+                    if let Ok(mut f) = File::create(output_filename) {
+                        for card in deck.cards {
+                            write!(f,"{}\n",card);
+                        }
+                    } else {
+                        println!("Couldn't open file '{}' for writing",output_filename)
+                    }
+                } else {
+                    for card in deck.cards {
+                        println!("{}",card)
+                    }
                 }
             }
         } else {
             let deck = datfile::parse_buffer(sl).unwrap();
-            if matches.is_present("echo") {
-                for card in deck.cards {
-                    println!("{}",card)
+            if echo {
+                if let Some(output_filename) = matches.value_of("OUTPUT") {
+                    if let Ok(mut f) = File::create(output_filename) {
+                        for card in deck.cards {
+                            write!(f,"{}\n",card);
+                        }
+                    } else {
+                        println!("Couldn't open file '{}' for writing",output_filename)
+                    }
+                } else {
+                    for card in deck.cards {
+                        println!("{}",card)
+                    }
                 }
             }
         }
