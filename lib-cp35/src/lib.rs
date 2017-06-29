7e6492327711551cbc197c8d86ebd4deeffe6fab
Andrew Burkett <burkett.andrew@gmail.com>
2017-06-29 11:24:22 -0700

    Add parse_line to python libs

--- lib-cp35/src/lib.rs
+++ lib-cp35/src/lib.rs
@@ -2,13 +2,22 @@ extern crate nastran;
 #[macro_use]
 extern crate cpython;
 
-use nastran::datfile::{maybe_any_field, Field};
-use cpython::{Python, PyResult, PyObject, ToPyObject, PythonObject, PyErr};
+use nastran::datfile::{self, maybe_any_field, Field};
+use cpython::{Python, PyResult, PyObject, ToPyObject, PythonObject, PyErr, PyList};
 use cpython::exc::BaseException;
 
 py_module_initializer!(nastranrs, initnastranrs, PyInit_nastranrs, |py, m| {
     try!(m.add(py, "__doc__", "This module is implemented in Rust."));
-    try!(m.add(py, "parse_field", py_fn!(py, parse_field(field: String))));
+    try!(m.add(
+        py,
+        "parse_field",
+        py_fn!(py, parse_field(field: String)),
+    ));
+    try!(m.add(
+        py,
+        "parse_line",
+        py_fn!(py, parse_line(line: String)),
+    ));
     Ok(())
 });
 
@@ -28,5 +37,43 @@ pub fn parse_field(py: Python, field: String) -> PyResult<PyObject> {
             let msg = format!("Couldn't parse field '{}'", field);
             Err(PyErr::new::<ParseFieldError, String>(py, msg))
         }
+    }
+}
+
+fn field_to_pyobject(py: Python,field: Field) -> PyObject {
+    match field {
+        Field::Blank => "".to_py_object(py).into_object(),
+        Field::Int(v) => v.to_py_object(py).into_object(),
+        Field::Float(v) => v.to_py_object(py).into_object(),
+        Field::Double(v) => v.to_py_object(py).into_object(),
+        Field::Continuation(v) => v.trim().to_py_object(py).into_object(),
+        Field::DoubleContinuation(v) => v.trim().to_py_object(py).into_object(),
+        Field::String(v) => v.trim().to_py_object(py).into_object(),
+        Field::DoubleString(v) => v.trim().to_py_object(py).into_object(),
+    }
+}
+
+
+pub fn parse_line(py: Python, field: String) -> PyResult<PyList> {
+    match datfile::parse_line(field.as_bytes()) {
+        Ok(card) => {
+            let mut list = vec![];
+            let obj = field_to_pyobject(py,card.first);
+            list.push(obj);
+            for field in card.fields {
+                let obj = field_to_pyobject(py, field);
+                list.push(obj);
+            }
+            if let Some(cont) = card.continuation {
+                list.push(cont.to_py_object(py).into_object());
+            };
+            Ok(PyList::new(py,list.as_slice()))
+        }
+        Err(e) => {
+            println!("{}",e);
+            let msg = format!("Couldn't parse line '{}'", field);
+            Err(PyErr::new::<ParseFieldError, String>(py, msg))
+        }
+
     }
 }
